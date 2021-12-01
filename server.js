ce50cd419c80c500742b12f7e2e3af7cfcff7492
JadElHakim <jh.jadhakim@gmail.com>
2021-12-01 21:44:20 +0200

    new page on login

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-let express = require('express');
+const express = require('express');
 let app = express();
-let bodyParser = require('body-parser');
-let ejs= require('ejs');
+const bodyParser = require('body-parser');
+const ejs= require('ejs');
 const multer= require('multer');
+const mongoose = require('mongoose');
 
+app.set('view engine', 'ejs');
 
 app.use( bodyParser.json() ); 
 app.use(bodyParser.urlencoded({     
@@ -19,11 +21,25 @@ app.use(function(req, res, next) {
 
 app.use(express.static(__dirname));
 
+//fetch the page when the user is not logged in
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/Home.html");
 });
 
+//fetch the dynamic page when the user is logged im
+app.get("/user",function(req,res){
+    let name = req.query.name;
+    ejs.renderFile(__dirname+"/views/Home.ejs",{name: name},function(error,data){
+        res.send(data);
+    });
+});
 
+//fetch the page with the logged in user personal books.
+app.get("/personalbooks",function(req,res){
+    ejs.renderFile(__dirname+"/views/personalbooks.ejs",function(error,data){
+        res.send(data);
+    });
+});
 let server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
