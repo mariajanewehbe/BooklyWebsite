4f0f0db3d80c3a0c8ff612324e19891db2ebcf9e
Maria Jane Wehbe <mariajane.wehbe@lau.edu>
2021-12-10 00:41:37 +0200

    database commit

--- personalbooks.js
+++ personalbooks.js
@@ -1,18 +1,18 @@
 function addToggle() {
-    var x = document.getElementById("addForm");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  var x = document.getElementById("addForm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
+}
 
-function getPName(){
-    let name ="";
-    if (auth2.isSignedIn.get()) {
-        var profile = auth2.currentUser.get().getBasicProfile();
-       profile.getName();
-    }
-    document.querySelector("#txt_name").setAttribute("value", name);
+function getPName() {
+  let name = "";
+  if (auth2.isSignedIn.get()) {
+    var profile = auth2.currentUser.get().getBasicProfile();
+    profile.getName();
+  }
+  document.querySelector("#txt_name").setAttribute("value", name);
 }
 getPName();
--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-const express = require('express');
+const express = require("express");
 let app = express();
-const bodyParser = require('body-parser');
-const ejs= require('ejs');
-const multer= require('multer');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const multer = require("multer");
+const mongoose = require("mongoose");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use( bodyParser.json() ); 
-app.use(bodyParser.urlencoded({     
-        extended: true
-      })); 
-      
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.use(express.static(__dirname));
@@ -25,96 +29,166 @@ let name = "";
 
 // ************************MAIN PAGES***********************************//
 //fetch the page when the user is not logged in
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/Home.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/Home.html");
 });
 //fetch the dynamic page when the user is logged in or when home is looked for
-app.get("/Home",function(req,res){
-    if(name == ""){name = req.query.name;}
-    
-    let title = "Bookly: Read With Us";
-    ejs.renderFile(__dirname+"/views/Home.ejs",{name: name , title: title},function(error,data){
-        res.send(data);
-    });
+app.get("/Home", function (req, res) {
+  if (name == "") {
+    name = req.query.name;
+  }
+
+  let title = "Bookly: Read With Us";
+  ejs.renderFile(
+    __dirname + "/views/Home.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
 });
-app.get("/Genres",function(req,res){
-    let title = "Genres";
-    ejs.renderFile(__dirname+"/views/Genres.ejs",{name: name,title: title},function(error,data){
-        res.send(data);
-    });
+app.get("/Genres", function (req, res) {
+  let title = "Genres";
+  ejs.renderFile(
+    __dirname + "/views/Genres.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
 });
-app.get("/Recommendations",function(req,res){
-    let title = "Recommendations";
-    ejs.renderFile(__dirname+"/views/Recommendations.ejs",{name: name,title: title},function(error,data){
-        res.send(data);
-    });
+app.get("/Recommendations", function (req, res) {
+  let title = "Recommendations";
+  ejs.renderFile(
+    __dirname + "/views/Recommendations.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
 });
 
 //fetch the page with the logged in user personal books.
-app.get("/personalbooks",function(req,res){
-    let title = "Personal Books";
-    ejs.renderFile(__dirname+"/views/personalbooks.ejs",{name: name, title: title},function(error,data){
-        res.send(data);
-    });
+app.get("/personalbooks", function (req, res) {
+  let title = "Personal Books";
+  ejs.renderFile(
+    __dirname + "/views/personalbooks.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
 });
 // ************************MAIN PAGES***********************************//
 
+// ************************SUB PAGES***********************************//
+app.get("/Fantasy", function (req, res) {
+  let title = "Fantasy";
+  ejs.renderFile(
+    __dirname + "/views/Fantasy.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
+});
 
+app.get("/Graphic", function (req, res) {
+  let title = "Graphic Novels";
+  ejs.renderFile(
+    __dirname + "/views/Graphic.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
+});
+app.get("/Romance", function (req, res) {
+  let title = "Romance";
+  ejs.renderFile(
+    __dirname + "/views/Romance.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
+});
+app.get("/Scifi", function (req, res) {
+  let title = "SCi-fi";
+  ejs.renderFile(
+    __dirname + "/views/Scifi.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
+});
+app.get("/Thriller", function (req, res) {
+  let title = "Thriller";
+  ejs.renderFile(
+    __dirname + "/views/Thriller.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
+});
+app.get("/Mystery", function (req, res) {
+  let title = "Mystery";
+  ejs.renderFile(
+    __dirname + "/views/Mystery.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
+});
+app.get("/Search", function (req, res) {
+  let title = "Search";
+  ejs.renderFile(
+    __dirname + "/views/Search.ejs",
+    { name: name, title: title },
+    function (error, data) {
+      res.send(data);
+    }
+  );
+});
 
 // ************************SUB PAGES***********************************//
-app.get("/Fantasy",function(req,res){
-    let title = "Fantasy";
-    ejs.renderFile(__dirname+"/views/Fantasy.ejs",{name: name,title: title},function(error,data){
-        res.send(data);
-    });
-});
 
-app.get("/Graphic",function(req,res){
-    let title = "Graphic Novels";
-    ejs.renderFile(__dirname+"/views/Graphic.ejs",{name: name,title: title},function(error,data){
-        res.send(data);
-    });
-});
-app.get("/Romance",function(req,res){
-    let title = "Romance";
-    ejs.renderFile(__dirname+"/views/Romance.ejs",{name: name,title: title},function(error,data){
-        res.send(data);
-    });
-});
-app.get("/Scifi",function(req,res){
-    let title = "SCi-fi";
-    ejs.renderFile(__dirname+"/views/Scifi.ejs",{name: name,title: title},function(error,data){
-        res.send(data);
-    });
-});
-app.get("/Thriller",function(req,res){
-    let title = "Thriller";
-    ejs.renderFile(__dirname+"/views/Thriller.ejs",{name: name,title: title},function(error,data){
-        res.send(data);
-    });
-});
- app.get("/Mystery",function(req,res){
-        let title = "Mystery";
-        ejs.renderFile(__dirname+"/views/Mystery.ejs",{name: name,title: title},function(error,data){
-            res.send(data);
-    });
-});
-app.get("/Search",function(req,res){
-    let title = "Search";
-    ejs.renderFile(__dirname+"/views/Search.ejs",{name: name,title: title},function(error,data){
-        res.send(data);
-    });
-});
+// ************************DATABASE***********************************//
+mongoose.connect(
+    "mongodb+srv://bookly:bookly123@cluster0.qgrk0.mongodb.net/BookDb",
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true },
+  );
 
-// ************************SUB PAGES***********************************//
+  const bookSchema = {
+    title: String,
+    author: String,
+    review: String
+  };
 
+  const Book = mongoose.model("Book", bookSchema);
 
+  app.get('/', function(req, res){
+   res.sendFile("personalbooks.ejs");
+  });
 
-
-
+  app.post("/", function(req, res) {
+      let newBoook = new Book({
+          title: req.body.title,
+          author: req.body.author,
+          review: req.body.review
+      });
+      newBoook.save();
+      res.redirect("/");
+  })
+  
+  // ************************DATABASE***********************************//
 
 let server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("listening on 8081");
- });
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("listening on 8081");
+});
